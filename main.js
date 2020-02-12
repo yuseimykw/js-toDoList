22b27ceb8ee7a355e72bbfd698bf541d284c4cb8
mykw ysi <miyakawaysi@outlook.jp>
2020-02-12 18:03:04 +0900

    hinanText削除

--- main.js
+++ main.js
@@ -8,19 +8,17 @@ document.getElementById("inputBtn").addEventListener("click", function () {
     removeBtn.textContent = "削除"
 
 
-    ///ボタンが押されたあとのinputText
-    document.getElementById("inputText").value = ""
+    // ///ボタンが押されたあとのinputText
+    // document.getElementById("inputText").value = ""
 
     //編集ボタン
     const editBtn = document.createElement("button")
     editBtn.textContent = "編集"
     editBtn.addEventListener("click", function () {
         const editItem = document.createElement("input")
-        const hinanText = toDoText.previousSibling
         editItem.type = "text"
         toDoText.remove()
         editItem.type = "text"
-        editItem.value = hinanText
         toDoItem.insertBefore(editItem, editBtn)
         this.remove()
 
